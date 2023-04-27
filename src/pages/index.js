48c5f205e458823b7fc39575a1c421bfcde740dc
Vinny <vinnyshipley@gmail.com>
2023-04-27 01:07:15 +0000

    more basic css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,13 @@ import Link from 'next/link';
 export default function BlogLandingPage() {
 	return (
 		<>
-			<main className='bg-gray-500 mt-45'>
+			<main className='bg-gray-500 mt-45 py-10'>
 				<div class='title'>
-					<h1 className='bg-gray-400 text-4xl text-center'>
+					<h1 className='bg-gray-400 text-4xl text-center py-10'>
 						Vinny Shipley Personal Website
 					</h1>
 				</div>
-				<h2 className='bg-gray-300 m-50'>
+				<h2 className='bg-gray-300 m-50 py-10'>
 					<Link href='/blog'>To Blog Page</Link>
 					<br></br>
 					<Link href='/projects'>To Project Page</Link>
