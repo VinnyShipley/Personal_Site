9df932b06e2a3b4ab9d1093f1cfce376609d4cd4
Vinny <vinnyshipley@gmail.com>
2023-05-16 00:53:06 +0000

    styling back-ish on main page

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,21 +1,20 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 function Header() {
 	return (
-		<div>
-			<h1 className='bg-primary text-4xl text-center  ml-16'>
-				Vinny Shipley Personal Website
-			</h1>
+		<div className='bg-secondary pt-4'>
+				<h1 className='bg-primary text-4xl text-center ml-16 pb-2'>
+					Vinny Shipley Personal Website
+				</h1>
+			
 
-			<nav className='flex justify-center space-x-20 focus:bg-black'>
+			<nav className='flex justify-center space-x-20 border-black'>
+				<Link href='/blog'>To Blog Page</Link>
 
-						<Link href='/blog'>To Blog Page</Link>
-
-						<Link href='/projects'>To Projects Page</Link>
-
-					</nav>
+				<Link href='/projects'>To Projects Page</Link>
+			</nav>
 		</div>
 	);
-};
+}
 
 export default Header;
