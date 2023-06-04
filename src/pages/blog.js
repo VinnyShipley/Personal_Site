b9a0a0fe307028c6583df4596f4446f0e70af876
Vinny <vinnyshipley@gmail.com>
2023-06-04 17:13:01 +0000

    blog posts getting pulled from db and displayed on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,56 @@
-import Link from 'next/link';
-import SideBar from './sidebar';
+// import SideBar from './sidebar';
+// import { getBlogPosts } from '../../lib/mongo/movies';
 
-export default function BlogLandingPage() {
-	return (
-		<>
-			<SideBar />
-			<h1 className='text-4xl text-center text-bold bg-primary m-50 py-10'>
-				Blog Landing Page
-			</h1>
-			<h2 className='bg-secondary m-50 py-10 text-center'>
-				Blog portion coming soon!
-			</h2>
-		</>
-	);
+// async function fetchPosts() {
+// 	const posts = await getBlogPosts()
+// 	if (!posts) throw new Error('Failed to fetch blog posts')
+	
+// 	return posts
+// }
+
+// export async function BlogLandingPage() {
+// 	const { blogPosts } = await fetchPosts()
+
+// 	return (
+// 		<>
+// 			<SideBar />
+// 			<h1 className='text-4xl text-center text-bold bg-primary m-50 py-10'>
+// 				Blog Landing Page
+// 			</h1>
+// 			<ul>
+// 				{blogPosts.map((post) => (
+// 					<li key={post._id}>{post.body}</li>
+// 				))}
+// 			</ul>
+// 		</>
+// 	);
+// }
+
+import SideBar from './sidebar';
+import { getBlogPosts } from '../../lib/mongo/movies';
+
+export default function BlogPage({posts}) {
+  return (
+    <>
+      <SideBar />
+      <h1 className="text-4xl text-center text-bold bg-primary m-50 py-10">
+        Blog Landing Page
+      </h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post._id}>{post.body}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const { blogPosts } = await getBlogPosts();
+
+  return {
+    props: {
+      posts: blogPosts,
+    },
+  };
 }
