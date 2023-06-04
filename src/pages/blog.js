7b0b399249890119b2be487fc5f5f9fb9f900f1f
Vinny <vinnyshipley@gmail.com>
2023-06-04 17:20:46 +0000

    cleanup time on aisle blog post, amirite?

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,31 +1,3 @@
-// import SideBar from './sidebar';
-// import { getBlogPosts } from '../../lib/mongo/movies';
-
-// async function fetchPosts() {
-// 	const posts = await getBlogPosts()
-// 	if (!posts) throw new Error('Failed to fetch blog posts')
-	
-// 	return posts
-// }
-
-// export async function BlogLandingPage() {
-// 	const { blogPosts } = await fetchPosts()
-
-// 	return (
-// 		<>
-// 			<SideBar />
-// 			<h1 className='text-4xl text-center text-bold bg-primary m-50 py-10'>
-// 				Blog Landing Page
-// 			</h1>
-// 			<ul>
-// 				{blogPosts.map((post) => (
-// 					<li key={post._id}>{post.body}</li>
-// 				))}
-// 			</ul>
-// 		</>
-// 	);
-// }
-
 import SideBar from './sidebar';
 import { getBlogPosts } from '../../lib/mongo/movies';
 
