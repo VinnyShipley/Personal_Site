53ee8c28a879284a8c2019ff31059aa991b0c98d
Vinny <vinnyshipley@gmail.com>
2023-09-04 18:55:24 +0000

    more styling main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,20 @@ function MainLandingPage() {
 			<main>
 				<div>
 					<div className='bg-basic py-9'>
-						<h1 className='text-4xl text-center ml-16 pb-2.5'>
-						Welcome to the Personal Website for Vinny Shipley
+						<h1 className='text-4xl text-center ml-16 pb-2.5 '>
+							Welcome to the Personal Website for Vinny Shipley
 						</h1>
 					</div>
 					<SideBar />
-					<h2 className='flex justify-center text-3xl pt-8'>About</h2>
-					<p className='flex justify-center pt-8 text-2xl\'>Follow the journey of a bartender turned software engineer on the hunt for employment in the tech sphere</p>
-				</div>
-				<div id="project-card" className='mt-60'>
+					<div id='home-about' className='space-y-14'>
+						<h2 className='flex justify-center text-3xl  font-bold mt-14'>
+							About
+						</h2>
+						<p className='flex justify-center pt-8 text-2xl\'>
+							Follow the journey of a bartender turned software engineer on the
+							hunt for employment in the tech sphere
+						</p>
+					</div>
 				</div>
 			</main>
 		</>
