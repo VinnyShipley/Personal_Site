ad427fd53f86f249c38e72e68148d5b10914c8fd
Vinny <vinnyshipley@gmail.com>
2023-09-04 19:10:24 +0000

    styling and technologies used added to project card

--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -1,14 +1,19 @@
 const projectArray = [
-  {title: 'Budget App', description: 'A budget calculator to figure out a fair split of finances between multiple people with different incomes and any number of expenses', link: 'https://github.com/VinnyShipley/Budget-App'}
-]
+  {title: 'Budget App', 
+  description: 'A budget calculator to figure out a fair split of finances between multiple people with different incomes and any number of expenses', 
+  link: 'https://github.com/VinnyShipley/Budget-App', 
+  techs: 'Pyton, Django',
+  }];
 
 const ProjectCard = () => (
-  <div className="flex justify-center">
+  <div className="flex justify-center mt-16">
     {projectArray.map((item) => (
-      <div className="bg-accent rounded-lg py-8 mt-12" key={item.title}>
+      <div className="bg-accent rounded-lg py-8 mt-12 text-2xl" key={item.title}>
         <p className="flex justify-center">{item.title}</p>
         <br/>
         <p className="flex justify-center px-4">{item.description}</p>
+        <br/>
+        <p className="flex justify-center px-4">Technologies Used: {item.techs}</p>
         <br/>
         <a className="flex justify-center underline" href={item.link}>Click here to view the Github repository</a>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import ProjectCard from '@/components/projectcard';
 import SideBar from '../components/sidebar';
 
 function MainLandingPage() {
@@ -16,9 +15,8 @@ function MainLandingPage() {
 						<h2 className='flex justify-center text-3xl  font-bold mt-14'>
 							About
 						</h2>
-						<p className='flex justify-center pt-8 text-2xl\'>
-							Follow the journey of a bartender turned software engineer on the
-							hunt for employment in the tech sphere
+						<p className='flex justify-center pt-8 text-2xl'>
+							Follow the journey of a bartender turned software engineer on the hunt for employment in the tech sphere
 						</p>
 					</div>
 				</div>
